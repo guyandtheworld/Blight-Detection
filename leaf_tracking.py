57179a485134fd479fa6928f85734e9e00e88d57
Adarsh S <4darshofficial@gmail.com>
2017-09-27 23:50:45 +0530

    Add brown detection and optimise code

--- leaf_tracking.py
+++ leaf_tracking.py
@@ -1,11 +1,13 @@
+from scipy import ndimage
 import numpy as np
 import imutils
 import cv2
  
 
-def detect_brown():
-    lower_brown = [10, 100, 20]
-    upper_brown = [20, 255, 200]
+def detect_contour(mask):
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)[-2]
+    return cnts
 
 def detect_leaf(frame, x, y, w, h, diseased):
     color = "green"
@@ -17,28 +19,32 @@ def detect_leaf(frame, x, y, w, h, diseased):
     h = h + h/5
     cv2.rectangle(frame, (x, y), (x+w, y+h), hsv_color, 2)
 
-def detect_disease(image, brown):
-    cv2.circle(image, brown, 5, (0, 0, 255), -1)
+def detect_disease(cnts_brown):
+    if len(cnts_brown) > 0:
+        c = max(cnts_brown, key=cv2.contourArea)
+        return True
+    return False
 
-def draw_contours(cnts, frame):
-    c = max(cnts, key=cv2.contourArea)
+def draw_contours(cnts_green, cnts_brown, frame):
+    c = max(cnts_green , key=cv2.contourArea)
     ((x, y), radius) = cv2.minEnclosingCircle(c)
-    (x, y, w, h) = cv2.boundingRect(c)
-    M = cv2.moments(c)
-    brown = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    (x, y, w, h) = cv2.boundingRect(c)    
     diseased = True
     if radius > 10:
-        detect_leaf(frame, x, y, w, h, False)
-        if diseased:
-            detect_disease(frame, brown)
+        diseased = detect_disease(cnts_brown)
+        detect_leaf(frame, x, y, w, h, diseased)
 
 def main():
 
     greenLower = (29, 86, 6)
     
     greenUpper = (64, 255, 255)
-    
-    camera = cv2.VideoCapture(0)
+
+    brownLower = (10, 100, 20)
+
+    brownUpper = (20, 255, 200)
+
+    camera = cv2.VideoCapture(1)
     
     while True:
         (_, frame) = camera.read()
@@ -46,16 +52,26 @@ def main():
         frame = imutils.resize(frame, width=600)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        mask = cv2.inRange(hsv, greenLower, greenUpper)
+        mask_green = cv2.inRange(hsv, greenLower, greenUpper)
+        mask_brown = cv2.inRange(hsv, brownLower, brownUpper)
 
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
+        mask_green = cv2.erode(mask_green, None, iterations=2)
+        mask_brown = cv2.erode(mask_brown, None, iterations=2)
+        mask_green = cv2.dilate(mask_green, None, iterations=2)
+        mask_brown = cv2.dilate(mask_brown, None, iterations=2)
 
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)[-2]
+        cnts_green = detect_contour(mask_green.copy())
+        cnts_brown = detect_contour(mask_brown.copy())
 
-        if len(cnts) > 0:
-            draw_contours(cnts, frame)
+        if len(cnts_green) > 0:
+            draw_contours(cnts_green, cnts_brown, frame)
+
+        # Testing
+        if len(cnts_green) > 0:
+            mask = mask_green + mask_brown
+            res = cv2.bitwise_and(frame, frame, mask=mask)
+            
+            cv2.imshow('res',res)
 
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
